4d08b976a2b356f5e77683b473fd13433aeea859
manish.d.yadav <manish.d.yadav@accenture.com>
2015-10-05 15:54:12 +0530

    test

--- Gitdemo/src/com/test/Test.java
+++ Gitdemo/src/com/test/Test.java
@@ -4,10 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("hello");
-		System.out.println("hello 2");
-		System.out.println("hello 3");
-		System.out.println("hello 3");
-		System.out.println("hello 3");
+			System.out.println("hello");
+				System.out.println("hello");
+		
 	}
 
 }
